55bf71a0520266e3e84bac1d7a7cdb1eb60f6c2f
adrian <ap.brown011@gmail.com>
2022-03-10 17:45:32 -0500

    final push

--- scripts/rsync_action.py
+++ scripts/rsync_action.py
@@ -1,10 +1,17 @@
 import os
+from dotenv import load_dotenv
 
-def remote_sync_pull():
-    pass
+load_dotenv()
 
-def remote_sync_push():
-    pass
+def rsync_push():
+    source_dir = input("current directory: ")
+    destination_dir = input("destination directory: ")
+    return os.system(f"rsync -a ~{source_dir} {os.getenv('username')}@{os.getenv('host')}:{destination_dir}")
+
+def rsync_pull():
+    source_dir = input("remote dir: ")
+    destination_dir = input("local dir: ")
+    return os.system(f"rsync -a {os.getenv('username')}@{os.getenv('host')}:{source_dir} ~{destination_dir}")
 
 if __name__ == "__main__":
-    os.system("echo 'hello world'")
+    rsync_pull()
