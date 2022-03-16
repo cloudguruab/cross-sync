8e72837e6ee3a9a5df9dd380d6a389bc68d21672
adrian <ap.brown011@gmail.com>
2022-03-16 11:08:41 -0400

    update to scripts

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Tue March 8th, 2022
+
+Specify the following in an env file:
+```yaml
+host=
+port=
+password=
+```
--- scripts/rsync_action.py
+++ scripts/rsync_action.py
@@ -6,12 +6,17 @@ load_dotenv()
 def rsync_push():
     source_dir = input("current directory: ")
     destination_dir = input("destination directory: ")
-    return os.system(f"rsync -a ~{source_dir} {os.getenv('username')}@{os.getenv('host')}:{destination_dir}")
+    return os.system(f"rsync -a ~{source_dir} root@{os.getenv('host')}:{destination_dir}")
 
 def rsync_pull():
     source_dir = input("remote dir: ")
     destination_dir = input("local dir: ")
-    return os.system(f"rsync -a {os.getenv('username')}@{os.getenv('host')}:{source_dir} ~{destination_dir}")
+    return os.system(f"rsync -a root@{os.getenv('host')}:{source_dir} ~{destination_dir}")
+
+def main():
+    q = input("Pulling from host [Y/n]: ")
+    if q.strip().lower() == "y": rsync_pull()
+    else: rsync_push()
 
 if __name__ == "__main__":
-    rsync_pull()
+    main()
